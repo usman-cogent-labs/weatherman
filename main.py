fdc2f4ae9acd68bb3bb26059f74ba03216147a24
usman-cogent-labs <109211422+usman-cogent-labs@users.noreply.github.com>
2023-03-12 21:11:04 +0500

    single quotes changed to double quotes

--- main.py
+++ main.py
@@ -18,55 +18,56 @@ def trim_list_elements(arr):
 def parse_weather_files(weather_filenames):
     data = list()
     for filename in weather_filenames:
-        with open('weatherfiles/' + filename, 'r') as file_data:
+        with open("weatherfiles/" + filename, "r") as file_data:
             csvreader = csv.DictReader(file_data)
             records = list()
             for row in csvreader:
-                raw_date = row['PKT'].split('-')
+                print(row)
+                raw_date = row["PKT"].split("-")
                 year = int(raw_date[0])
                 month = int(raw_date[1])
                 day = int(raw_date[2])
                 date = datetime.datetime(year, month, day)
                 records.append({
-                    "date": row['PKT'],
-                    "highest_temperature": row['Max TemperatureC'],
-                    "lowest_temperature": row['Min TemperatureC'],
-                    "mean_humidity": row[' Mean Humidity'],
-                    "max_humidity": row['Max Humidity'],
+                    "date": row["PKT"],
+                    "highest_temperature": row["Max TemperatureC"],
+                    "lowest_temperature": row["Min TemperatureC"],
+                    "mean_humidity": row[" Mean Humidity"],
+                    "max_humidity": row["Max Humidity"],
                 })
-            data.append({"month": date.strftime('%B'), "records": records})
+            data.append({"month": date.strftime("%B"), "records": records})
     return data
 
 
 def get_formatted_date(date):
-    year, month, day = date.split('-')
-    return datetime.date(int(year), int(month), int(day)).strftime('%B %d')
+    year, month, day = date.split("-")
+    return datetime.date(int(year), int(month), int(day)).strftime("%B %d")
 
 
 def print_yearly_data(data):
-    highest = {"temperature": 0, "date": ''}
-    lowest = {"temperature": 1000, "date": ''}
-    humidest = {"temperature": 0, "date": ''}
+    highest = {"temperature": 0, "date": ""}
+    lowest = {"temperature": 1000, "date": ""}
+    humidest = {"temperature": 0, "date": ""}
 
     for month_data in data:
-        for record in month_data['records']:
-            if record['highest_temperature'].isnumeric() \
-                    and int(record['highest_temperature']) > highest['temperature']:
-                highest['temperature'] = int(record['highest_temperature'])
-                highest['date'] = get_formatted_date(record['date'])
-            if record['lowest_temperature'].isnumeric() and int(record['lowest_temperature']) < lowest['temperature']:
-                lowest["temperature"] = int(record['lowest_temperature'])
-                lowest['date'] = get_formatted_date(record['date'])
-            if record['max_humidity'].isnumeric() and int(record['max_humidity']) > humidest['temperature']:
-                humidest['temperature'] = int(record['max_humidity'])
-                humidest['date'] = get_formatted_date(record['date'])
-    print(f"Highest: {highest['temperature']}C on {highest['date']}")
-    print(f"Lowest: {lowest['temperature']}C on {lowest['date']}")
-    print(f"Humidity: {humidest['temperature']}% on {humidest['date']}")
+        for record in month_data["records"]:
+            if record["highest_temperature"].isnumeric() \
+                    and int(record["highest_temperature"]) > highest["temperature"]:
+                highest["temperature"] = int(record["highest_temperature"])
+                highest["date"] = get_formatted_date(record["date"])
+            if record["lowest_temperature"].isnumeric() and int(record["lowest_temperature"]) < lowest["temperature"]:
+                lowest["temperature"] = int(record["lowest_temperature"])
+                lowest["date"] = get_formatted_date(record["date"])
+            if record["max_humidity"].isnumeric() and int(record["max_humidity"]) > humidest["temperature"]:
+                humidest["temperature"] = int(record["max_humidity"])
+                humidest["date"] = get_formatted_date(record["date"])
+    print(f"Highest: {highest["temperature"]}C on {highest["date"]}")
+    print(f"Lowest: {lowest["temperature"]}C on {lowest["date"]}")
+    print(f"Humidity: {humidest["temperature"]}% on {humidest["date"]}")
 
 
 def check_month_in_command(command_data):
-    return False if len(command_data.split('/')) == 1 else True
+    return False if len(command_data.split("/")) == 1 else True
 
 
 def get_month_data_from_data(data, month):
@@ -86,24 +87,24 @@ def print_monthly_average_data(data, command_data):
         "sum": 0,
         "count": 0
     }
-    for record in data['records']:
-        if record['highest_temperature'] != '':
-            highest_temp_data['sum'] += int(record['highest_temperature'])
+    for record in data["records"]:
+        if record["highest_temperature"] != "":
+            highest_temp_data["sum"] += int(record["highest_temperature"])
             highest_temp_data["count"] += 1
-        if record['lowest_temperature'] != '':
-            lowest_temp_data["sum"] += int(record['lowest_temperature'])
+        if record["lowest_temperature"] != "":
+            lowest_temp_data["sum"] += int(record["lowest_temperature"])
             lowest_temp_data["count"] += 1
-        if record['mean_humidity'] != '':
-            humidity_temp_data["sum"] += int(record['mean_humidity'])
+        if record["mean_humidity"] != "":
+            humidity_temp_data["sum"] += int(record["mean_humidity"])
             humidity_temp_data["count"] += 1
 
     highest_average = highest_temp_data["sum"] // highest_temp_data["count"]
     lowest_average = lowest_temp_data["sum"] // lowest_temp_data["count"]
     average_humidity = humidity_temp_data["sum"] // humidity_temp_data["count"]
 
-    print(f'Highest Average: {highest_average}')
-    print(f'Lowest Average: {lowest_average}')
-    print(f'Average Mean Humidity: {average_humidity}')
+    print(f"Highest Average: {highest_average}")
+    print(f"Lowest Average: {lowest_average}")
+    print(f"Average Mean Humidity: {average_humidity}")
 
 
 def draw_month_graph(data, month):
@@ -113,7 +114,7 @@ def draw_month_graph(data, month):
 def get_year_month_from_command(command_data):
     if not check_month_in_command(command_data):
         return command_data
-    year, month = command_data.split('/')
+    year, month = command_data.split("/")
     month = calendar.month_name[int(month)]
     return year, month
 
@@ -145,7 +146,7 @@ def print_monthly_data(monthly_data):
 
 
 def get_files_from_directory():
-    return os.listdir('weatherfiles/')
+    return os.listdir("weatherfiles/")
 
 
 def execute_command(command_type, command_data):
