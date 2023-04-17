6a8b41b841945702cf8917970f34a58a1eb41aab
usman-cogent-labs <109211422+usman-cogent-labs@users.noreply.github.com>
2023-04-18 01:24:55 +0500

    Code refactored

--- main.py
+++ main.py
@@ -14,10 +14,6 @@ parser.add_argument('-e', '--extreme', type=int, help='the year to filter files 
 args = parser.parse_args()
 
 
-def get_year_file_names(weather_reports, year):
-    return [weather_report for weather_report in weather_reports if year in weather_report]
-
-
 def parse_weather_files(weather_filenames):
     readings = []
     for filename in weather_filenames:
@@ -72,29 +68,31 @@ def print_yearly_readings(readings):
     print(f"Humidity: {humidest['temperature']}% on {humidest['date']}")
 
 
-def check_month_in_command(command_line_arguments):
-    return False if len(command_line_arguments.split("/")) == 1 else True
+def print_monthly_average_readings(monthly_readings):
+    highest_temp_data = {
+        "sum": 0,
+        "count": 0
+    }
+    lowest_temp_data = {
+        "sum": 0,
+        "count": 0
+    }
+    humidity_temp_data = {
+        "sum": 0,
+        "count": 0
+    }
 
-
-def get_monthly_readings_from_yearly_readings(yearly_readings, month):
-    return next((item for item in yearly_readings if item["month"] == month), None)
-
-
-def print_monthly_average_readings(data):
-    highest_temp_data = {}
-    lowest_temp_data = {}
-    humidity_temp_data = {}
-
-    for record in data["records"]:
-        if record["highest_temperature"] != "":
-            highest_temp_data["sum"] += int(record["highest_temperature"])
-            highest_temp_data["count"] += 1
-        if record["lowest_temperature"] != "":
-            lowest_temp_data["sum"] += int(record["lowest_temperature"])
-            lowest_temp_data["count"] += 1
-        if record["mean_humidity"] != "":
-            humidity_temp_data["sum"] += int(record["mean_humidity"])
-            humidity_temp_data["count"] += 1
+    for monthly_reading in monthly_readings:
+        for record in monthly_reading["records"]:
+            if record["highest_temperature"] != "":
+                highest_temp_data["sum"] += int(record["highest_temperature"])
+                highest_temp_data["count"] += 1
+            if record["lowest_temperature"] != "":
+                lowest_temp_data["sum"] += int(record["lowest_temperature"])
+                lowest_temp_data["count"] += 1
+            if record["mean_humidity"] != "":
+                humidity_temp_data["sum"] += int(record["mean_humidity"])
+                humidity_temp_data["count"] += 1
 
     highest_average = highest_temp_data["sum"] // highest_temp_data["count"]
     lowest_average = lowest_temp_data["sum"] // lowest_temp_data["count"]
@@ -103,31 +101,6 @@ def print_monthly_average_readings(data):
     print(f"Highest Average: {highest_average}")
     print(f"Lowest Average: {lowest_average}")
     print(f"Average Mean Humidity: {average_humidity}")
-
-
-def get_year_month_from_command(command_line_arguments):
-    if not check_month_in_command(command_line_arguments):
-        return command_line_arguments
-
-    year, month = command_line_arguments.split("/")
-    month = calendar.month_name[int(month)]
-
-    return year, month
-
-
-def get_yearly_readings(files, command_line_arguments):
-    year, month = get_year_month_from_command(command_line_arguments)\
-        if len(get_year_month_from_command(command_line_arguments)) == 2\
-        else (get_year_month_from_command(command_line_arguments), None)
-    file_names = get_year_file_names(files, year)
-
-    return parse_weather_files(file_names)
-
-
-def get_monthly_weather_readings(files, command_line_arguments):
-    year, month = get_year_month_from_command(command_line_arguments)
-    yearly_readings = get_yearly_readings(files, command_line_arguments)
-    return get_monthly_readings_from_yearly_readings(yearly_readings, month)
 
 
 def print_monthly_readings(monthly_readings):
@@ -140,22 +113,26 @@ def print_monthly_readings(monthly_readings):
     print(Style.RESET_ALL)
 
 
-def get_files_from_directory():
-    return os.listdir("weatherfiles/")
+def get_file_names(file_path):
+    return glob.glob(os.path.join('weatherfiles/', f'*_{file_path}_*'))
+
+
+def get_monthly_readings(command_type):
+    year, month = command_type.split('/')
+    month_name = calendar.month_name[int(month)][:3]
+    return parse_weather_files(get_file_names(f'{year}_{month_name}'))
 
 
 def execute_command():
-    files = get_files_from_directory()
-    print(args)
     if args.extreme:
         file_names = glob.glob(os.path.join('weatherfiles/', f'*_{args.extreme}_*'))
         yearly_readings = parse_weather_files(file_names)
         print_yearly_readings(yearly_readings)
     if args.average:
-        monthly_readings = get_monthly_weather_readings(files)
+        monthly_readings = get_monthly_readings(args.average)
         print_monthly_average_readings(monthly_readings)
     if args.current:
-        monthly_readings = get_monthly_weather_readings(files)
+        monthly_readings = get_monthly_readings(args.current)
         print_monthly_readings(monthly_readings)
 
 
